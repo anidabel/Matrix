e0035603b3883bbed9f6bb0893872a665ffd1d3b
Darya Belkina <dariya.belkina@mail.ru>
2021-04-18 13:38:37 +0300

    Добавьте файлы проекта.

---
+++ Matrix.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+    public class Matrix
+    {
+        private int KolN;
+        private int KolM;
+        private double[,] mat;
+
+        public void SetRandMatr()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < GetKolN; i++)
+                for (int j = 0; j < GetKolN; j++)
+                    mat[i, j] = rand.NextDouble();
+        }
+        public void SetKolN(int n)
+        {
+            KolN = n;
+        }
+        public void SetKolM(int m)
+        {
+            KolM = m;
+        }
+        public int GetKolN
+        {
+            get { return KolN; }
+        }
+        public int GetKolM
+        {
+            get { return KolM; }
+        }
+        public double GetElem(int i, int j)
+        {
+            return mat[i, j];
+        }
+        public Matrix(int n, int m)
+        {
+            this.KolN = n;
+            this.KolM = m;
+            this.mat = new double[n, m];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    mat[i, j] = 0.0;
+        }
+        public Matrix(double[,] matr)
+        {
+            this.KolN = matr.GetLength(0);
+            this.KolM = matr.GetLength(1);
+            this.mat = matr;
+        }
+        public Matrix(Matrix m)
+        {
+            KolN = m.GetKolN;
+            KolM = m.GetKolM;
+            mat = m.mat;
+        }
+        public double this[int n, int m]
+        {
+            get
+            {
+                if (n < 0 || n > KolN || m < 0 || m > KolM)
+                    throw new Exception("Выход за границы матрицы");
+                return mat[n, m];
+            }
+            // set { mat[n, m] = value; }
+        }
+        public static Matrix operator +(Matrix m1, Matrix m2)
+        {
+            if (!Matrix.CompareSum(m1, m2))
+                throw new Exception("Невозможно проссумировать матрицы");
+            Matrix Res = new Matrix(m1.GetKolN, m1.GetKolM);
+            for (int i = 0; i < m1.GetKolN; i++)
+                for (int j = 0; j < m1.GetKolM; j++)
+                    Res.mat[i, j] = m1[i, j] + m2[i, j];
+            return Res;
+        }
+        public static Matrix operator -(Matrix m1, Matrix m2)
+        {
+            if (!Matrix.CompareSum(m1, m2))
+                throw new Exception("Невозможно проссумировать матрицы");
+            Matrix Res = new Matrix(m1.GetKolN, m1.GetKolM);
+            for (int i = 0; i < m1.GetKolN; i++)
+                for (int j = 0; j < m1.GetKolM; j++)
+                    Res.mat[i, j] = m1[i, j] - m2[i, j];
+            return Res;
+        }
+        public static Matrix operator *(Matrix m, double d)
+        {
+            Matrix res = new Matrix(m.GetKolN, m.GetKolM);
+            for (int i = 0; i < m.GetKolN; i++)
+                for (int j = 0; j < m.GetKolM; j++)
+                    res.mat[i, j] = m[i, j] * d;
+            return res;
+        }
+        public static Matrix operator *(double d, Matrix m)
+        {
+            Matrix res = m * d;
+            return res;
+        }
+        public static Matrix operator /(Matrix m, double d)
+        {
+            if (d == 0)
+                throw new Exception("Деление на ноль");
+            Matrix res = new Matrix(m.GetKolN, m.GetKolM);
+            for (int i = 0; i < m.GetKolN; i++)
+                for (int j = 0; j < m.GetKolM; j++)
+                    res.mat[i, j] = m.mat[i, j] / d;
+            return res;
+        }
+        public static Matrix operator *(Matrix m1, Matrix m2)
+        {
+            if (m1.GetKolM != m2.GetKolN)
+                throw new Exception("Умножение матриц друг на друга невозможно");
+            double tmp;
+            Matrix res = new Matrix(m1.GetKolN, m2.GetKolM);
+            for (int i = 0; i < m1.GetKolN; i++)
+                for (int j = 0; j < m2.GetKolM; j++)
+                {
+
+                    tmp = res.mat[i, j];
+                    for (int k = 0; k < res.GetKolN; k++)
+                        tmp += m1[i, k] * m2[k, j];
+                    res.mat[i, j] = tmp;
+                }
+            return res;
+        }
+        public static bool CompareSum(Matrix m1, Matrix m2)
+        {
+            if (m1.GetKolN == m2.GetKolN && m1.GetKolN == m2.GetKolM)
+                return true;
+            else
+                return false;
+        }
+        public bool Squared() //квадратная
+        {
+            if (KolN == KolM)
+                return true;
+            else
+                return false;
+        }
+        public bool Diagonal() //диагональная
+        {
+            if (!this.Squared())
+                return false;
+            for (int i = 0; i < KolN; i++)
+                for (int j = 0; j < KolM; i++)
+                    if (this[i, j] != 0 && i != j)
+                        return false;
+            return true;
+        }
+        public bool Zero()   //нулевая
+        {
+            for (int i = 0; i < KolN; i++)
+                for (int j = 0; j < KolM; j++)
+                    if (this[i, j] != 0)
+                        return false;
+            return true;
+        }
+        public bool One() //единичная
+        {
+            for (int i = 0; i < KolN; i++)
+                for (int j = 0; j < KolM; j++)
+                    if (this[i, j] != 1)
+                        return false;
+            return true;
+        }
+        public bool Simmetr()   //симметричная
+        {
+            if (!this.Squared())
+                return false;
+            for (int i = 0; i < KolN; i++)
+                for (int j = 0; j < KolM; j++)
+                    if (this[i, j] != this[j, i])
+                        return false;
+            return true;
+        }
+        public bool VTr()   //верхняя треугольная
+        {
+            if (!this.Squared())
+                return false;
+            for (int i = 1; i < KolN; i++)
+                for (int j = 0; j < i; j++)
+                    if (this[i, j] != 0)
+                        return false;
+            return true;
+        }
+        public bool NTr()     //нижняя треугольная
+        {
+            if (!this.Squared())
+                return false;
+            for (int i = 0; i < KolN - 1; i++)
+                for (int j = i + 1; j < KolM; j++)
+                    if (this[i, j] != 0)
+                        return false;
+            return true;
+        }
+        public static bool operator ==(Matrix m1, Matrix m2)  //сравнение на равенство 
+        {
+            if (m1.GetKolM != m2.GetKolM || m1.GetKolN != m2.GetKolN)
+                return false;
+            for (int i = 0; i < m1.GetKolN; i++)
+                for (int j = 0; j < m1.GetKolM; j++)
+                    if (m1[i, j] != m2[i, j])
+                        return false;
+            return true;
+        }
+        public static bool operator !=(Matrix m1, Matrix m2)   // не равенство
+        {
+            if (m1 == m2)
+                return false;
+            else
+                return true;
+        }
+        public void Show()
+        {
+            for (int i = 0; i < KolN; i++)
+            {
+                for (int j = 0; j < KolM; j++)
+                    Console.Write("\t" + this[i, j]);
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("\n Для упрощения работы вывода матриц, они будут во многих примерах квадратными с фиксированной размерностью\n\n");
+                Console.WriteLine("\n Задание матрицы A \n");
+                int n = 4;
+                Matrix A = new Matrix(new double[n, n]);
+                A.SetRandMatr();
+                A.Show();
+                Console.WriteLine("\n Задание матрицы В\n");
+                Matrix B = new Matrix(new double[n, n]);
+                B.SetRandMatr();
+                B.Show();
+                Console.WriteLine("Сравнение матриц А и В");
+                if (A == B)
+                    Console.WriteLine("     Матрицы равны");
+                else
+                    Console.WriteLine("     Матрицы не равны");
+                Console.WriteLine("Создадим матрицу С равную матрице А и сравним их");
+                Matrix C = new Matrix(A);
+                if (C == A)
+                    Console.WriteLine("     Матрицы равны");
+                else
+                    Console.WriteLine("     Матрицы не равны");
+                Console.WriteLine("Произведем различные проверки матрицы А, используя реализованные нами методы");
+                if (A.Squared())
+                    Console.WriteLine("     Матрица квадратная");
+                else
+                    Console.WriteLine("     Матрица не квадратная");
+                if (A.Diagonal())
+                    Console.WriteLine("     Матрица диагональная");
+                else
+                    Console.WriteLine("     Матрица не диагональная");
+                if (A.Zero())
+                    Console.WriteLine("     Матрица нулевая");
+                else
+                    Console.WriteLine("     Матрица не нулевая");
+                if (A.One())
+                    Console.WriteLine("     Матрица единичная");
+                else
+                    Console.WriteLine("     Матрица не единичная");
+                if (A.Simmetr())
+                    Console.WriteLine("     Матрица симметричная");
+                else
+                    Console.WriteLine("     Матрица не симметричная");
+                if (A.VTr())
+                    Console.WriteLine("     Матрица верхняя треугольная");
+                else
+                    Console.WriteLine("     Матрица не верхняя треугольная");
+                if (A.NTr())
+                    Console.WriteLine("     Матрица нижняя треугольная");
+                else
+                    Console.WriteLine("     Матрица не нижняя треугольная");
+                Console.WriteLine("     Найдём сумму матриц А и В");
+                (A + B).Show();
+                Console.WriteLine("     Найдём произведение матриц А и В");
+                (A * B).Show();
+                Console.WriteLine("     Найдём разницу матриц А и В");
+                (A - B).Show();
+                Console.WriteLine("     Найдём произведение матрицы А на 2");
+                (A * 2).Show();
+                Console.WriteLine("     Найдём частное матрицы А и 5");
+                (A / 5).Show();
+                Console.WriteLine("\n\nПрограмма заврешила свою работу!\n\n");
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение!");
+            }
+
+        }
+    }
+}
